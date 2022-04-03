a06090ee4182c30a34ba7bdd6ebb62c70266b4a3
1CarlosRenan <carlosrenan96@gmail.com>
2022-04-03 19:05:14 -0300

    Modal

---
+++ js/modules/modal.js
@@ -0,0 +1,25 @@
+export default function initModal() {
+    const botaoAbrir = document.querySelector('[data-modal="abrir"]');
+    const botaoFechar = document.querySelector('[data-modal="fechar"]');
+    const containerModal = document.querySelector('[data-modal="container"]');
+
+    if (botaoAbrir && botaoFechar && containerModal) {
+        function toogleModal(event) {
+            event.preventDefault();
+            containerModal.classList.toggle('ativo');
+        }
+
+        function cliqueForaModal(event) {
+            if (event.target === this) {
+                toogleModal(event);
+            }
+        }
+
+        botaoAbrir.addEventListener('click', toogleModal);
+        botaoFechar.addEventListener('click', toogleModal);
+        containerModal.addEventListener('click', cliqueForaModal);
+    }
+}
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initActiveAccordion from './modules/accordion.js';
-import initTabNav from './modules/tabnav.js'
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tootltip';
 
 initScrollSuave();
 initAnimacaoScroll();
 initActiveAccordion();
 initTabNav();
+initModal();
+initTooltip();
