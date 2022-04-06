10bef0ccc59e938ad7004e94dfa4479eb0422316
1CarlosRenan <carlosrenan96@gmail.com>
2022-04-05 21:33:26 -0300

    Dropdown Menu

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ import initActiveAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js'
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -11,3 +12,4 @@ initActiveAccordion();
 initTabNav();
 initModal();
 initTooltip();
+initDropdownMenu();
